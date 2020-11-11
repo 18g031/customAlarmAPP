1d8c04666e5c09bad84443cc554b3c2fcff08108
H.abe <x18g003@chiba-fjb.ac.jp>
2020-11-11 11:15:59 +0900

    test

--- app/src/main/java/com/example/alarmapp/MainActivity.java
+++ app/src/main/java/com/example/alarmapp/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -11,4 +14,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void TimeReference() {
+
+        // 現在日時を取得
+        Date date1 = new Date();
+        // System.out.println(date1); //Fri Aug 05 00:28:47 JST 2016
+
+        // 表示形式を指定
+        SimpleDateFormat sdformat1
+                = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        String fdate1 = sdformat1.format(date1);
+        System.out.println(fdate1); // 2016/08/05 00:28:47.646
+
+    }
+
 }
+
