2099955afaa3e46b8cb2c431e748d38115baeaf6
yamada <x18g038@chiba-fjb.ac.jp>
2020-11-17 11:49:06 +0900

    11/17 時計作成途中

---
+++ app/src/main/java/com/example/alarmapp/clock.java
@@ -0,0 +1,47 @@
+package com.example.alarmapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class clock extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.clock);
+        Intent intent = getIntent();
+
+        findViewById(R.id.enter).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timeConf(mhours(), mminutes());
+            }
+        });
+    }
+
+    //時を取得
+    private String mhours() {
+        EditText Ehour = (EditText) this.findViewById(R.id.hour);
+        String hour = Ehour.getText().toString();
+        return hour;
+    }
+
+    //分を取得
+    private String mminutes() {
+        EditText Eminute = (EditText) this.findViewById(R.id.minute);
+        String minute = Eminute.getText().toString();
+        return minute;
+    }
+
+    //設定した時刻を表示
+    private void timeConf(String h, String m) {
+        TextView confview = (TextView) this.findViewById(R.id.confview);
+        confview.setText("設定した時間は　" + h + "：" + m);
+    }
+
+}
