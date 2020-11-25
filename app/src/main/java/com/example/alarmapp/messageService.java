0291f544211acd11177546e8232371d83235aff8
yamada <x18g038@chiba-fjb.ac.jp>
2020-11-25 11:07:57 +0900

    11/19　時計作成

--- app/src/main/java/com/example/alarmapp/messageService.java
+++ app/src/main/java/com/example/alarmapp/messageService.java
@@ -5,27 +5,26 @@ import android.content.Intent;
 import android.os.IBinder;
 
 public class messageService extends Service{
-        //
-        public IBinder onBind(Intent intent)
-        {
-            return null;
-        }
-
-        public void onCreate()
-        {
-            Thread thread = new Thread(null, task, "sentMessageService");
-            thread.start();
-        }
-
-        Runnable task = new Runnable()
-        {
-            public void run() {
-                Intent messageBroadcast = new Intent();
-                messageBroadcast.setAction("activityAction");
-                sendBroadcast(messageBroadcast);
-                stopSelf();
-            }
-
-        };
+    //これがないとエラーになる（おまじない的な感じ）
+    public IBinder onBind(Intent intent)
+    {
+        return null;
     }
 
+    public void onCreate()
+    {
+        Thread thread = new Thread(null, task, "sentMessageService");
+        thread.start();
+    }
+
+    Runnable task = new Runnable()
+    {
+        public void run() {
+            Intent messageBroadcast = new Intent();
+            messageBroadcast.setAction("activityAction");
+            sendBroadcast(messageBroadcast);
+            stopSelf();
+        }
+
+    };
+}
