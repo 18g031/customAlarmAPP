9d3f334f30b7b265d0d34e96fbf31c7ffdd74a16
N.hanawa <x18g031@chiba-fjb.ac.jp>
2020-11-26 15:53:54 +0900

    基盤整備

---
+++ app/src/main/java/com/example/alarmapp/AdditionActivity.java
@@ -0,0 +1,15 @@
+package com.example.alarmapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.AlarmManager;
+
+//課金画面
+
+public class AdditionActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/example/alarmapp/RangeActivity.java
@@ -0,0 +1,15 @@
+package com.example.alarmapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.AlarmManager;
+
+//ランダム時間設定画面
+
+public class RangeActivity extends AppCompatActivity {
+}
