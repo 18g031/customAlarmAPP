ed9a1a57ca005a43d8f1db2277e38914ef2e4cfc
N.hanawa <x18g031@chiba-fjb.ac.jp>
2020-11-30 11:38:13 +0900

    コメント編集

--- app/src/main/java/com/example/alarmapp/HistoryActivity.java
+++ app/src/main/java/com/example/alarmapp/HistoryActivity.java
@@ -1,4 +1,10 @@
 package com.example.alarmapp;
 
-public class HistoryActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+
+//履歴画面
+
+public class HistoryActivity extends AppCompatActivity {
 }
