98f0a8ef329f942225da4fd1c9b081152646d46e
O.Ooki <x18g010@chiba-fjb.ac.jp>
2020-12-04 12:41:30 +0900

    AlarmList　コメント追加

--- app/src/main/java/com/example/alarmapp/MainActivity.java
+++ app/src/main/java/com/example/alarmapp/MainActivity.java
@@ -6,6 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 //スタート画面の動作
 
@@ -18,6 +25,26 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Intent intent = getIntent();
+
+
+        String[] Time ={"8:00 9:00","8:10 9:10"};
+        String[] name ={"arlarm,announce","arlarm,announce"};
+
+        List<Map<String, String>> data = new ArrayList<Map<String, String>>();
+        for (int i=0; i<Time.length; i++){
+            Map<String, String> item = new HashMap<String, String>();
+            item.put("Subject", Time[i]);
+            item.put("Comment", name[0]);
+            data.add(item);
+        }
+
+        ListView alList = findViewById(R.id.alList);
+        //List<String> menuList = new ArrayList<>();
+        SimpleAdapter adapter = new SimpleAdapter(this, data, android.R.layout.simple_list_item_2,
+                new String[] { "Subject", "Comment" },
+                new int[] { android.R.id.text1, android.R.id.text2});
+        alList.setAdapter(adapter);
+
     }
 
     public void onClickConfig(View view) {
