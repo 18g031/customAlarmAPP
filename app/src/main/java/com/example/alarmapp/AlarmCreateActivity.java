894114c3c772e3c90c33ec0b22b3021e5c72fa34
okadakairi <kairinggg@mail.com>
2020-12-09 03:08:12 +0900

    1208 timepickerが動かしたい

--- app/src/main/java/com/example/alarmapp/AlarmCreateActivity.java
+++ app/src/main/java/com/example/alarmapp/AlarmCreateActivity.java
@@ -1,21 +1,83 @@
 package com.example.alarmapp;
 
+import android.app.TimePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.text.Editable;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+
 import android.app.AlarmManager;
+import android.widget.TimePicker;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 //アラーム、アナウンスの編集画面
 
 public class AlarmCreateActivity extends AppCompatActivity {
 
+/////////////////////////////////////
+    ///////////////////////////
+
+    TextView tvTimer;
+    int tHour,tMinute;
+
+    ///////////////////////////
+/////////////////////////////////////
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+/////////////////////////////////////
+    ///////////////////////////
+
+        setContentView(R.layout.clock);
+        tvTimer = findViewById(R.id.tv_timer);
+
+        tvTimer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                TimePickerDialog timePickerDialog = new TimePickerDialog(
+                        AlarmCreateActivity.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker View, int hourOfDay, int minute) {
+                                tHour = hourOfDay;
+                                tMinute = minute;
+                                String time = tHour + ":" + tMinute;
+                                SimpleDateFormat f24Hours = new SimpleDateFormat(
+                                        "HH:mm"
+                                );
+                                try {
+                                    Date date = f24Hours.parse(time);
+                                    SimpleDateFormat f12Hours = new SimpleDateFormat(
+                                            "hh:mm aa"
+                                    );
+                                    tvTimer.setText(f12Hours.format(date));
+                                } catch (ParseException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        },12,0,false
+                );
+                timePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                timePickerDialog.updateTime(tHour,tMinute);
+                timePickerDialog.show();
+            }
+        });
+
+    ///////////////////////////
+/////////////////////////////////////
+
         setContentView(R.layout.clock);
         Intent intent = getIntent();
         String value1 = intent.getStringExtra("ALKEY");//MainActivityのリストから画面遷移した時のデータ
