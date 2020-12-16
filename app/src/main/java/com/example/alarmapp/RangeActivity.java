d44213377ab8ec71e2d9f50f0452f7e5206d63de
yamada <x18g038@chiba-fjb.ac.jp>
2020-12-16 13:12:56 +0900

    1216

--- app/src/main/java/com/example/alarmapp/RangeActivity.java
+++ app/src/main/java/com/example/alarmapp/RangeActivity.java
@@ -7,7 +7,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.Random;
 
 
@@ -15,21 +19,37 @@ import java.util.Random;
 //ランダム時間設定画面
 //ランダム数生成
 public class RangeActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.clock);
+        try {
+
+            SimpleDateFormat sdf = new SimpleDateFormat("mm");//date型に変えるためのインスタンス
+            Date date = sdf.parse(mminutes());//ｓｔｒDateをdate型に変換
+
+            Calendar keisan = Calendar.getInstance();
+            keisan.setTime(date);
+            date.add(Calendar.MINUTE, randomValue);
+
+
+
+
+        }catch (ParseException e){
+
+        }
+            }
+
+
     Random random = new Random();
     int randomValue = random.nextInt(30);
 
-    private String mhours() {
-        TextView ahour = (TextView) this.findViewById(R.id.hour);
-        String hour = ahour.getText().toString();
-        return hour;
-    }
 
 
-    private String mminutes() {
-        TextView aminutes = (TextView) this.findViewById(R.id.minute);
+    private String mminutes() { TextView aminutes = (TextView) this.findViewById(R.id.minute);
         String minute = aminutes.getText().toString();
         return minute;
         //test
+
     }
-    
+
 }
