68005baa3b4c2d4364d07fe05f7039bc310e1a8a
yamada <x18g038@chiba-fjb.ac.jp>
2021-01-28 14:50:43 +0900

    1/28　まだ終わってない

--- app/src/main/java/com/example/alarmapp/activity/AlarmCreateActivity.java
+++ app/src/main/java/com/example/alarmapp/activity/AlarmCreateActivity.java
@@ -415,7 +415,24 @@ public class AlarmCreateActivity extends AppCompatActivity {
 
                 Intent intent2 = new Intent(AlarmCreateActivity.this, MainActivity.class); //保存を押したらメインにもどる
                 startActivity(intent2);
+
+                Calendar calendar2 = Calendar.getInstance();
+                calendar.setTimeInMillis(System.currentTimeMillis());
+                calendar.add(Calendar.SECOND, 10);
+                scheduleNotification("10秒後に届く通知です", calendar2);
             }
+                private void scheduleNotification(String content, Calendar calendar){
+                    Intent notificationIntent = new Intent(this, AlarmBackGround.class);
+                    notificationIntent.putExtra(AlarmReceiver.NOTIFICATION_ID, 1);
+                    notificationIntent.putExtra(AlarmReceiver.NOTIFICATION_CONTENT, content);
+                    PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+                    AlarmManager alarmManager = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+                    alarmManager.setExact(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
+                }
+            }
+
+        }
         });
         //削除メソッド
         findViewById(R.id.del).setOnClickListener(new View.OnClickListener() {
